46e7457e967f090b429043a1e1cdba831e8b0ee9
Abhijeet Vighne <abhijeet.vighne@cohesity.com>
2025-03-01 11:36:43 -0800

    Initial Flutter project setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart'; // Create this file next
+import 'home_screen.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
