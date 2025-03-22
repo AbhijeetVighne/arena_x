f63a46f3bb216c930d5cbceb3f12660530a42962
AbhijeetVighne <85132577+AbhijeetVighne@users.noreply.github.com>
2025-03-23 01:04:25 +0530

    Update login_screen.dart

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,7 @@
+// Redundant file, this file is not being used anywhere
+
+
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
